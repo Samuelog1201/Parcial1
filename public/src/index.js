59a84ffbdc62c265517d307085579118aeca9751
Samuel Ortiz Guevara <1105362447@u.icesi.edu.co>
2025-03-28 18:11:39 -0500

    new-container-component

--- public/src/index.js
+++ public/src/index.js
@@ -1,3 +1,19 @@
 class Container extends HTMLElement {
-    
+	constructor() {
+		super();
+		this.attachShadow({ mode: 'open' });
+	}
+
+	connectedCallback() {
+		this.render();
+	}
+
+	render() {
+		this.shadowRoot.innerHTML = `
+        
+        `   
+	}
 }
+
+customElements.define('Container', Container);
+export default Container;
